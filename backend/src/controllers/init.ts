5108d402430f2d9459558249b2ee739413b5b2cb
Harsh <harsh990921992@gmail.com>
2026-02-02 12:56:44 +0530

    init command logic defined

--- backend/src/controllers/init.ts
+++ backend/src/controllers/init.ts
@@ -1,7 +1,21 @@
+import { promises as fs } from "fs";
+import path from 'path';
 
+const initRepo = async() =>{
+    const repoPath = path.resolve(process.cwd(),".myGit");
+    const commitsPath = path.join(repoPath,"commits");
 
-const initRepo = () =>{
-    console.log("repop initialized");
+    try{
+       await fs.mkdir(repoPath,{recursive:true})
+       await fs.mkdir(commitsPath ,{recursive:true})
+       await fs.writeFile(
+        path.join(repoPath,'config.json'),
+        JSON.stringify({bucket:process.env.S3_BUCKET})
+       )
+       console.log("repo initialized");
+    }catch(err){ 
+      console.error("error insializing the reposetory", err);
+    }
 }
 
 export {initRepo};
