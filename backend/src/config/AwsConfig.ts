42266a0c19a6c793cd6e1febe12613813d3fa0ad
Harsh <harsh990921992@gmail.com>
2026-02-04 20:51:03 +0530

    push function added

--- backend/src/config/AwsConfig.ts
+++ backend/src/config/AwsConfig.ts
@@ -1,8 +1,16 @@
 import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+dotenv.config();
 
 AWS.config.update({region:"ap-south-1"});
 
-const S3  = new AWS.S3();
-const S3_BUCKET = "mygit-folder-bucket";
+const S3  = new AWS.S3({
+    accessKeyId:process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+}
+
+);
+const S3_BUCKET = "mygit-folder-storage";
 
 export {S3,S3_BUCKET}
--- backend/src/controllers/push.ts
+++ backend/src/controllers/push.ts
@@ -1,7 +1,38 @@
+import { promises as fs } from "fs";
+import path from 'path';
+import {S3 , S3_BUCKET} from '../config/AwsConfig.js';
 
+const pushRepo = async() =>{
+    const repoPath = path.resolve(process.cwd(),'.myGit');
+    const commitsPath = path.join(repoPath,'commits');
 
-const pushRepo = () =>{
-    console.log("push function called");
+    try{
+
+        const commitDirs = await fs.readdir(commitsPath);
+
+        for(const commitDir of commitDirs){
+            const commitpath = path.join(commitsPath,commitDir);
+            
+            const files = await fs.readdir(commitpath);
+            for(const file of files){
+                 const filePath = path.join(commitpath,file);
+                 const fileContent = await fs.readFile(filePath);
+                 
+                 const params = {
+                    Bucket: S3_BUCKET,
+                    Key: `commits/${commitDir}/${file}`,
+                    Body: fileContent
+                 };
+                 
+                 await S3.upload(params).promise();
+            }
+        }
+       
+        console.log('all commits pushed to s3');
+
+    }catch(err){
+        console.error("Error pushing in S3: ",err);
+    }
+
 }
-
 export {pushRepo};
